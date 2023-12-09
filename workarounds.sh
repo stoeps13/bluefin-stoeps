c075329d36d125ed2510d2cb63f2dd57a6ee5997
Christoph Stoettner <christoph.stoettner@stoeps.de>
2023-12-09 13:31:33 +0100

    fix: disable wayland in gdm

--- workarounds.sh
+++ workarounds.sh
@@ -4,3 +4,6 @@ set -oeux pipefail
 
 # alternatives cannot create symlinks on its own during a container build
 ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
+
+# Disable wayland on Lenovo T470 (prevents OBS crashes)
+/usr/libexec/gdm-runtime-config set daemon WaylandEnable false
